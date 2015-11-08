380d947db8afa03da38858a38ad08dadb28a6f18
Rickard Andersson <gonz@severnatazvezda.com>
2015-11-08 13:22:18 +0200

    Add `hash-ref*` macro to access nested hashes less verbosely
    gonz macro package added to image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM gonz/racket
 MAINTAINER Rickard Andersson <gonz@severnatazvezda.com>
 
+COPY gonz /gonz
+RUN yes | raco pkg install --link /gonz
+
 # Copy movie-star source to filesystem
 COPY src /wiki-page-src
 WORKDIR /wiki-page-src
--- src/search.rkt
+++ src/search.rkt
@@ -1,10 +1,12 @@
 #lang racket/base
 
 (require racket/port
-         
+
          net/http-client
          net/uri-codec
          json
+
+         gonz/nested-hash
 
          "access-info.rkt")
 
@@ -23,7 +25,7 @@
                    `(,(format "User-Agent: ~a"
                               user-agent)
                       "Content-Type: application/x-www-form-urlencoded")))
-  
+
   (read-json input-port))
 
 (define (article title
@@ -61,25 +63,21 @@
   (read-json input-port))
 
 (define (search->title results)
-  (hash-ref (car (hash-ref (hash-ref results
-                                     'query)
-                           'search))
+  (hash-ref (car (hash-ref* results
+                            'query 'search))
             'title))
 
 (define (extract->text results)
-  (define pages (hash-ref (hash-ref results
-                                    'query)
-                          'pages))
-  (hash-ref (hash-ref pages
-                      (car (hash-keys pages)))
-            'extract))
+  (define pages (hash-ref* results 'query 'pages))
+
+  (hash-ref (car (hash-values pages)) 'extract))
 
 (provide keyword->info)
 (define (keyword->info keyword)
   (define title (search->title (search keyword)))
 
   (define image-data (images title))
-  
+
   `#hash((title . ,title)
          (thumbnail . ,(images->thumbnail image-data))
          (original . ,(images->original image-data))
@@ -88,8 +86,8 @@
                                  (uri-encode title)))))
 
 (define (images title
-                 #:host [host wp/host]
-                 #:url [url wp/url/images])
+                #:host [host wp/host]
+                #:url [url wp/url/images])
   (define-values
     (response headers input-port)
     (http-sendrecv host
@@ -105,30 +103,14 @@
   (read-json input-port))
 
 (define (images->thumbnail results)
-  (define thumbnail
-    (hash-ref (car (hash-values (hash-ref (hash-ref results
-                                                    'query)
-                                          'pages)))
-              'thumbnail
-              '#hash()))
-  
-  (hash-ref thumbnail
-            'source
-            ""))
+  (define pages (car (hash-values (hash-ref* results 'query 'pages))))
+  (hash-ref* pages 'thumbnail 'source #:fail? ""))
 
 (define (images->original results)
-  (define original
-    (hash-ref (car (hash-values (hash-ref (hash-ref results
-                                                    'query)
-                                          'pages)))
-              'thumbnail
-              '#hash()))
-  
-  (hash-ref original
-            'original
-            ""))
+  (define pages (car (hash-values (hash-ref* results 'query 'pages))))
+  (hash-ref* pages 'thumbnail 'original #:fail? ""))
 
 (module+ main
   (require racket/pretty)
   (pretty-print
-    (images "Trello")))
+    (images "Weird Science")))
