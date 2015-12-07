c4db403d333b16844f5902e09bfd17d49a42bd6d
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-08 01:50:41 +0200

    Change indentation of web-start.rkt

--- src/web-start.rkt
+++ src/web-start.rkt
@@ -8,18 +8,17 @@
          web-server/dispatch
          web-server/page
          web-server/http/bindings
-         
+
          "slack.rkt"
          "search.rkt"
-         
          "jeapostrophe/threading-arrow.rkt")
 
 (define/page (slack-wiki-hook-response title)
   (response/full
-    200 #"Okay"
-    (current-seconds) TEXT/HTML-MIME-TYPE
-    '()
-    `(,(string->bytes/utf-8 (wiki-payload (keyword->info title))))))
+   200 #"Okay"
+   (current-seconds) TEXT/HTML-MIME-TYPE
+   '()
+   `(,(string->bytes/utf-8 (wiki-payload (keyword->info title))))))
 
 (define (slack-request/wiki-hook/post request)
   (slack-wiki-hook-response request
@@ -30,20 +29,20 @@
 
 (define/page (ping-page)
   (response/full
-    200 #"Okay"
-    (current-seconds) TEXT/HTML-MIME-TYPE
-    '()
-    `(,(string->bytes/utf-8 "Pong!"))))
+   200 #"Okay"
+   (current-seconds) TEXT/HTML-MIME-TYPE
+   '()
+   `(,(string->bytes/utf-8 "Pong!"))))
 
 (define (request/ping request)
   (ping-page request))
 
 (define-values (wikipedia-hook-hispatch wikipedia-page-url)
   (dispatch-rules
-    [("ping") request/ping]
-    [("wiki" "slack" "wiki-hook")
-     #:method "post"
-     slack-request/wiki-hook/post]))
+   [("ping") request/ping]
+   [("wiki" "slack" "wiki-hook")
+    #:method "post"
+    slack-request/wiki-hook/post]))
 
 (serve/servlet wikipedia-hook-hispatch
                #:port 8083
