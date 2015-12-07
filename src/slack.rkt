2f49df8b086c2997cae1547574b27e498554d25a
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-08 01:49:45 +0200

    Change indentation of slack.rkt

--- src/slack.rkt
+++ src/slack.rkt
@@ -7,12 +7,12 @@
   (define thumbnail (hash-ref info 'thumbnail))
   (define original (hash-ref info 'original))
   (jsexpr->string
-    `#hash((text . " ")
-           (attachments .
-                        (#hash((text . ,(hash-ref info 'extract))
-                               (fallback . ,(hash-ref info 'extract))
-                               (title . ,(hash-ref info 'title))
-                               (title_link . ,(hash-ref info 'link))
-                               (thumb_url . ,(hash-ref info 'thumbnail))
-                               (image_url . ,(hash-ref info 'original))))))))
+   `#hash((text . " ")
+          (attachments .
+                       (#hash((text . ,(hash-ref info 'extract))
+                              (fallback . ,(hash-ref info 'extract))
+                              (title . ,(hash-ref info 'title))
+                              (title_link . ,(hash-ref info 'link))
+                              (thumb_url . ,(hash-ref info 'thumbnail))
+                              (image_url . ,(hash-ref info 'original))))))))
 
